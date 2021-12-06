c8df01ee5ed4645b0a31674c6fbe0e11e9a81939
Aaron Huang <h499871809@gmail.com>
2021-12-06 18:34:05 +0800

    fix(ethereum): use ropsten (#44)

--- ethereum/multisig/deploy.js
+++ ethereum/multisig/deploy.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const Web3 = require('web3');
 const BigNumber = require('bignumber.js');
 const { Transaction } = require('@ethereumjs/tx');
 const { default: Common } = require('@ethereumjs/common');
-const { bufferToHex, pubToAddress, toChecksumAddress } = require('ethereumjs-util');
+const { bufferToHex, pubToAddress, toChecksumAddress, generateAddress, toBuffer } = require('ethereumjs-util');
 const { ec: EC } = require('elliptic');
 const ec = new EC('secp256k1');
 const Contract = require('web3-eth-contract');
@@ -10,6 +11,7 @@ const abi = require('../abis/multisig');
 const bytecode = require('../bytecodes/multisig');
 
 const contract = new Contract(abi);
+const { PRIVATE_KEY: privateKey } = process.env;
 
 const pubToBIP55Address = (key) => {
   const pair = ec.keyFromPublic(key, 'hex');
@@ -61,16 +63,16 @@ async function main({ web3, sender, pubs, common }) {
     },
     opts
   );
-
-  const signedTx = tx.sign(privateKey);
+  console.log('privateKey', privateKey);
+  const signedTx = tx.sign(toBuffer('0x' + privateKey));
   const serializedTx = signedTx.serialize();
-
-  // console.log('bufferToHex(serializedTx)', bufferToHex(serializedTx));
+  console.log('account  : ', bufferToHex(generateAddress(sender, toBuffer(nonce))));
+  console.log('bufferToHex(serializedTx) : ', bufferToHex(serializedTx));
   const hash = await sendTx(web3, bufferToHex(serializedTx));
   console.log('hash', hash);
 }
 
-const common = new Common({ chain: 'kovan' });
+const common = new Common({ chain: 'ropsten' });
 const sender = '0xd73d9AA55ABBd6CFbeD3e9Ad7f8Be2f6D83C70dC';
 const pubs = [
   '02f4147da97162a214dbe25828ee4c4acc4dc721cd0c15b2761b43ed0292ed82b5', // 0x370BA1dc25C07d0C77Ba9b83fcc75Fcc2a0aC243
@@ -78,7 +80,7 @@ const pubs = [
   '02f44bce3eecd274e7aa24ec975388d12905dfc670a99b16e1d968e6ab5f69b266',
 ];
 
-const provider = 'https://kovan.infura.io/v3/de9290b603fc4609a6f0a65e23e8c7d3';
+const provider = 'https://ropsten.infura.io/v3/de9290b603fc4609a6f0a65e23e8c7d3';
 const web3 = new Web3(provider);
 
 main({
