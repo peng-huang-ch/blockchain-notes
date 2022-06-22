85f46d9f9fa8fc79e26a3e3b4ca025fcf155a2fb
phcxc <h499871809@gmail.com>
2022-06-22 15:04:23 +0800

    feat: calc gnosis contract address

--- ethereum/multisig/gnosis/2_3/deploy.js
+++ ethereum/multisig/gnosis/2_3/deploy.js
@@ -5,7 +5,7 @@ const { default: Common, Chain, Hardfork } = require('@ethereumjs/common');
 const { getSafeSingletonDeployment, getProxyFactoryDeployment, getCompatibilityFallbackHandlerDeployment } = require('@gnosis.pm/safe-deployments');
 const Contract = require('web3-eth-contract');
 const { toBN, toHex } = require('web3-utils');
-const { bufferToHex, toBuffer } = require('ethereumjs-util');
+const { bufferToHex, toBuffer, addHexPrefix } = require('ethereumjs-util');
 const { sendTx } = require('../gnosis');
 
 async function deploy({
@@ -19,6 +19,7 @@ async function deploy({
 	const version = '1.3.0';
 	const released = true;
 	const filter = { version, released }
+
 	const safeSingleton = getSafeSingletonDeployment(filter);
 	const safeSingletonAddress = safeSingleton.networkAddresses[chain];
 	const safeSingletonABI = safeSingleton.abi;
@@ -45,12 +46,29 @@ async function deploy({
 	const safeSingletonContract = new Contract(safeSingletonABI);
 	const initializer = safeSingletonContract.methods.setup(...params).encodeABI();
 
-	var saltNonce = 1644659321895 || Date.now();
+	var saltNonce = 1655775220144 || Date.now();
 
 	const proxyFactoryContract = new Contract(proxyFactoryABI, proxyFactoryAddress);
 	proxyFactoryContract.setProvider(web3.currentProvider);
 
 	const input = proxyFactoryContract.methods.createProxyWithNonce(safeSingletonAddress, initializer, saltNonce).encodeABI();
+
+	var creationCode = await proxyFactoryContract.methods.proxyCreationCode().call();
+	const deploymentCode = ethers.utils.solidityPack(["bytes", "uint256"], [creationCode, safeSingletonAddress])
+
+	var salt = ethers.utils.solidityKeccak256(
+		["bytes32", "uint256"],
+		[ethers.utils.solidityKeccak256(["bytes"], [initializer]), saltNonce]
+	)
+	var address = ethers.utils.getCreate2Address(proxyFactoryAddress, salt, ethers.utils.keccak256(deploymentCode))
+	console.log('address 				: ', address);
+
+	const ep = ethers.providers.getDefaultProvider(provider);
+	let { maxFeePerGas, maxPriorityFeePerGas, gasPrice } = await ep.getFeeData();
+
+	gasPrice = gasPrice.toHexString();
+	maxFeePerGas = maxFeePerGas.toHexString();
+	maxPriorityFeePerGas = maxPriorityFeePerGas.toHexString();
 
 	const common = new Common({ chain: Chain.Rinkeby, hardfork: Hardfork.London });
 	const nonce = await web3.eth.getTransactionCount(sender);
@@ -63,7 +81,10 @@ async function deploy({
 		to: proxyFactoryAddress,
 		value: '0x00',
 	});
-
+	console.log('nonce : ', nonce);
+	console.log('gasLimit				: ', gasLimit);
+	console.log('maxFeePerGas			: ', maxFeePerGas);
+	console.log('maxPriorityFeePerGas	: ', maxPriorityFeePerGas);
 	const tx = Transaction.fromTxData(
 		{
 			maxFeePerGas,
@@ -76,9 +97,10 @@ async function deploy({
 		},
 		opts
 	);
-	console.log('tx serialized	: ', tx.serialize().toString('hex'));
-	console.log('tx hash 		: ', tx.getMessageToSign(true).toString('hex'));
-
+	console.log('tx data   : ', tx.toJSON())
+	console.log('tx serialized		: ', tx.serialize().toString('hex'));
+	console.log('message to sign    : ', tx.getMessageToSign(true).toString('hex'));
+	// return;
 	const signedTx = tx.sign(toBuffer(privateKey));
 
 	console.log('signed tx		: ', signedTx.toJSON());
@@ -86,17 +108,7 @@ async function deploy({
 	const serialized = signedTx.serialize();
 	const txid = signedTx.hash().toString('hex');
 	console.log('txid 		: ', txid);
-
-	var creationCode = await proxyFactoryContract.methods.proxyCreationCode().call();
-	const deploymentCode = ethers.utils.solidityPack(["bytes", "uint256"], [creationCode, safeSingletonAddress])
-
-	var salt = ethers.utils.solidityKeccak256(
-		["bytes32", "uint256"],
-		[ethers.utils.solidityKeccak256(["bytes"], [initializer]), saltNonce]
-	)
-	var address = ethers.utils.getCreate2Address(proxyFactoryAddress, salt, ethers.utils.keccak256(deploymentCode))
-	console.log('address 				: ', address);
-
+	return;
 	const hash = await sendTx(web3, bufferToHex(serialized));
 	console.log('hash', hash);
 }
@@ -105,13 +117,14 @@ const provider = 'https://rinkeby.infura.io/v3/de9290b603fc4609a6f0a65e23e8c7d3'
 const web3 = new Web3(provider);
 const threshold = 2;
 const owners = ['0x370BA1dc25C07d0C77Ba9b83fcc75Fcc2a0aC243', '0x2ab5d8164Cac7f006ED79817753Eb9C9CfeAa093', '0x8b67944F06DA3f68e0C97ff54d3b27D960134C63'];
+
 const sender = '0x0d5a689d6a973e945cbbfab37202a1788e5588e7';
 const privateKey = '';
 
 deploy({
+	web3,
 	threshold,
 	owners,
-	web3,
 	sender,
 	privateKey
 }).catch(console.error);
