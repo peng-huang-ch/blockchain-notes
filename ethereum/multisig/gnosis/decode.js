171baefc789d90648471c57420e107e275f66816
phcxc <h499871809@gmail.com>
2022-08-04 16:23:08 +0800

    refactor: remove useless code

--- ethereum/multisig/gnosis/decode.js
+++
@@ -1,6 +0,0 @@
-const { getSafeSingletonDeployment, getProxyFactoryDeployment } = require('@gnosis.pm/safe-deployments');
-const abiDecoder = require('abi-decoder');
-const { abi } = getSafeSingletonDeployment();
-console.log('abi : ', abi);
-abiDecoder.addABI(abi);
-console.log('abiDecoder', abiDecoder.getMethodIDs());
