ee351420d450e3a915cbaae632aad045a8638b02
peng-huang-ch <h499871809@gmail.com>
2024-12-04 14:38:39 +0800

    feat: solana price

---
+++ solana/subscribes/wsol-prices.ts
@@ -0,0 +1,30 @@
+import { Connection, PublicKey } from '@solana/web3.js';
+import { PoolInfoLayout, SqrtPriceMath } from '@raydium-io/raydium-sdk';
+
+// Establish new connect to devnet - websocket client connected to devnet will also be registered here
+const url = 'https://api.mainnet-beta.solana.com';
+
+// Raydium Liquidity Pool V4: WSOL-USDC
+const marketId = new PublicKey('8sLbNZoA1cfnvMJLPfp98ZLAnFSYCFApfJKMbiXNLwxj');
+const WSOL_MINT = new PublicKey('So11111111111111111111111111111111111111112');
+const USDC_MINT = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+
+const connection = new Connection(url, 'finalized');
+
+function v2() {
+  connection.onAccountChange(
+    marketId,
+    async (accountInfo) => {
+      const poolData = PoolInfoLayout.decode(accountInfo.data);
+      const price = SqrtPriceMath.sqrtPriceX64ToPrice(poolData.sqrtPriceX64, poolData.mintDecimalsA, poolData.mintDecimalsB).toFixed(6);
+      console.log(`WSOL/USDC:`, price);
+    },
+    'confirmed',
+  );
+}
+
+function main() {
+  v2();
+}
+
+main();
