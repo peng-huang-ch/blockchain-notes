149b0b7bd155d275853c215eca39a89f53cbcbb8
peng-huang-ch <h499871809@gmail.com>
2024-12-21 16:09:28 +0800

    feat: send sol

--- solana/spl-token/send-sol.ts
+++ solana/spl-token/send-sol.ts
@@ -1,73 +1,121 @@
 import 'dotenv/config';
+import assert from 'node:assert';
 import bs58 from 'bs58';
-import {
+import nacl from 'tweetnacl';
+import web3, {
   clusterApiUrl,
   Connection,
   PublicKey,
   Keypair,
-  Transaction,
   SystemProgram,
-  sendAndConfirmTransaction,
   LAMPORTS_PER_SOL,
+  TransactionMessage,
+  VersionedTransaction,
+  Commitment,
 } from '@solana/web3.js';
 
-var url = clusterApiUrl('testnet');
-var connection = new Connection(url, 'confirmed');
+const url = clusterApiUrl('devnet');
+const connection = new Connection(url, 'confirmed');
 console.log('Connection established', url);
 
-var aliceKey = process.env.SOL_ALICE_KEY;
-var bobKey = process.env.SOL_BOB_KEY;
+async function legacy(from: Keypair, to: Keypair, amount: number, commitment: Commitment) {
+  console.log('from publicKey', from.publicKey.toBase58());
+  console.log('to publicKey', to.publicKey.toBase58());
 
-const alice = Keypair.fromSecretKey(bs58.decode(aliceKey));
-const bob = Keypair.fromSecretKey(bs58.decode(bobKey));
-const multiSigners = [alice, bob];
-
-// TODO: complete it.
-async function main() {
-  // multisig address
-  var fromPubkey = new PublicKey('4WgeY3eNtTkUnmE85AxgJM17n1m3yAexnLjz12Fio2kk');
-  // const ownerAddress = fromPubkey;
-  var toPubkey = new PublicKey('3ejod8WdzSuQCXsY7jVvm74nBZr1JrxRnC1zZnxxBVWN');
-
-  // const transferInstruction = SystemProgram.transfer({
-  //   fromPubkey: fromAddress,
-  //   toPubkey: ownerAddress,
-  //   lamports: LAMPORTS_PER_SOL / 100,
-  // });
-  // const transaction = new Transaction().add(transferInstruction);
+  const fromPubkey = from.publicKey;
+  const toPubkey = to.publicKey;
 
   // Add transfer instruction to transaction
   const transferIx = SystemProgram.transfer({
     fromPubkey,
     toPubkey,
-    lamports: LAMPORTS_PER_SOL / 1,
-  });
-  const transaction = new Transaction({
-    feePayer: alice.publicKey,
+    lamports: amount,
   });
 
+  const recentBlockhash = await connection.getLatestBlockhash();
+  const transaction = new web3.Transaction({
+    recentBlockhash: recentBlockhash.blockhash,
+    feePayer: fromPubkey,
+  });
   transaction.add(transferIx);
-  // transaction.feePayer = fromPubkey;
 
-  // Sign transaction, broadcast, and confirm
-  const signature = await sendAndConfirmTransaction(connection, transaction, [alice, ...multiSigners], {
-    commitment: 'singleGossip',
-    preflightCommitment: 'singleGossip',
+  const transactionBuffer = transaction.serializeMessage();
+  const signature = nacl.sign.detached(transactionBuffer, from.secretKey);
+
+  transaction.addSignature(fromPubkey, Buffer.from(signature));
+
+  let isVerifiedSignature = transaction.verifySignatures();
+  console.log(`The signatures were verified: ${isVerifiedSignature}`);
+
+  // The signatures were verified: true
+
+  const rawTransaction = transaction.serialize();
+  const txid = await connection.sendRawTransaction(rawTransaction, {
+    // skipPreflight: true,
+    preflightCommitment: commitment,
+    maxRetries: 2,
   });
-  console.log('SIGNATURE', signature);
-
-  // // Transfer the sol to the "toAddress"
-  // signature = await transfer(
-  //   connection,
-  //   payerWallet,
-  //   fromAddress,
-  //   toAddress, //
-  //   ownerAddress,
-  //   10 ** 1,
-  //   multiSigners,
-  // );
-
-  console.log('transfer tx:', signature);
+  assert.strictEqual(txid, bs58.encode(signature), 'txid should be the same as signature');
+  console.log('txid', txid);
+  return txid;
 }
 
-// main().catch(console.error);
+async function versioned(from: Keypair, to: Keypair, amount: number, commitment: Commitment) {
+  console.log('from publicKey', from.publicKey.toBase58());
+  console.log('to publicKey', to.publicKey.toBase58());
+
+  const fromPubkey = from.publicKey;
+  const toPubkey = to.publicKey;
+  const feePayer = from.publicKey;
+
+  // Add transfer instruction to transaction
+  const transferIx = SystemProgram.transfer({
+    fromPubkey,
+    toPubkey,
+    lamports: amount,
+  });
+
+  const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+
+  const messageV0 = new TransactionMessage({
+    payerKey: feePayer,
+    recentBlockhash: blockhash,
+    instructions: [transferIx], // note this is an array of instructions
+  }).compileToV0Message();
+
+  const transactionV0 = new VersionedTransaction(messageV0);
+
+  const msg = messageV0.serialize();
+  const signature = nacl.sign.detached(msg, from.secretKey);
+  transactionV0.addSignature(feePayer, signature);
+
+  // The signatures were verified: true
+  const isVerifiedSignature = nacl.sign.detached.verify(msg, signature, new PublicKey(feePayer).toBuffer());
+  console.log(`The signatures were verified: ${isVerifiedSignature}`);
+
+  // send transaction
+  const raw = transactionV0.serialize();
+  const txid = await connection.sendRawTransaction(raw, {
+    // skipPreflight: true,
+    preflightCommitment: commitment,
+    maxRetries: 2,
+  });
+
+  assert.strictEqual(txid, bs58.encode(signature), 'txid should be the same as signature');
+  console.log('txid', txid);
+  return txid;
+}
+
+async function main() {
+  const aliceKey = process.env.SOL_ALICE_KEY;
+  const bobKey = process.env.SOL_BOB_KEY;
+  const from = Keypair.fromSecretKey(bs58.decode(aliceKey));
+  const to = Keypair.fromSecretKey(bs58.decode(bobKey));
+  const amount = LAMPORTS_PER_SOL / 100;
+  const commitment = 'confirmed';
+
+  await legacy(from, to, amount, commitment);
+  await versioned(from, to, amount, commitment);
+}
+
+main().catch(console.error);
